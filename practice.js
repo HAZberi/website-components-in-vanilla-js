426ebd8cce845c129970aef106e39c67f6580f4c
HAZberi <mhazuberi@gmail.com>
2020-11-02 16:13:26 -0700

    dom practice 2

--- practice.js
+++ practice.js
@@ -55,3 +55,50 @@ document
   });
 
 //Also see explanation on innerHTML vs insertAdjacentHTML
+
+//Styles
+
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+
+//to get the value of current styles as a string
+console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).height);
+
+//a use case for computed style with parseInt or parseFloat
+message.style.height = Number.parseFloat(getComputedStyle(message).height) + 40 + 'px';
+console.log(getComputedStyle(message).height);
+
+//changing css variables or custom csss
+
+//document.documentElement.style.setProperty('--color-primary','orangered');
+
+
+//Attributes
+const logo = document.querySelector('.nav__logo');
+//to access Standard HTML attributes
+console.log(logo.alt);
+console.log(logo.src);
+console.log(logo.className);
+
+//to access and setting Non-Standard HTML attributes
+console.log(logo.designer);
+console.log(logo.setAttribute('designer', 'HazBeri'));
+console.log(logo.getAttribute('designer'));
+
+//difference between src absolute and relative paths
+console.log(logo.src);//absolute
+console.log(logo.getAttribute('src'));//relative
+
+console.log(document.querySelector('.btn--show-modal').href);//absolute
+console.log(document.querySelector('.btn--show-modal').getAttribute('href'));//relative
+
+//Data Attributes
+console.log(document.querySelector('.nav__logo').dataset.versionNumber);
+console.log(logo.getAttribute('data-version-number'));
+
+//Classes
+logo.classList.add('c', 'j', 'k');
+logo.classList.remove('c', 'j', 'k');
+logo.classList.toggle('c', 'j', 'k');
+logo.classList.contains('c', 'j', 'k'); //not includes like in arrays
